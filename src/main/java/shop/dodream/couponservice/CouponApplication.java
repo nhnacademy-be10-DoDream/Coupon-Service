78dbd0907d287179f16b4a436933783869ba050b
chosun-nhn17 <12160569@inha.edu>
2025-06-22 13:27:40 +0900

    feat: project init

---
+++ src/main/java/shop/dodream/couponservice/CouponApplication.java
@@ -0,0 +1,13 @@
+package shop.dodream.couponservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shop/dodream/couponservice/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.dodream.couponservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
