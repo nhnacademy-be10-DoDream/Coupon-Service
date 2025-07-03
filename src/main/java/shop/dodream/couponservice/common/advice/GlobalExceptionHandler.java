79f8aa3bc9486c040abed5a1342bcfd3789774f1
chosun-nhn17 <12160569@inha.edu>
2025-07-03 12:58:06 +0900

    refactor: fix advice
    - advice 한글 - 영문 수정
    - set property 삭제
    
    #24

--- src/main/java/shop/dodream/couponservice/common/advice/GlobalExceptionHandler.java
+++ src/main/java/shop/dodream/couponservice/common/advice/GlobalExceptionHandler.java
@@ -1,14 +1,17 @@
 package shop.dodream.couponservice.common.advice;
 
 import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import shop.dodream.couponservice.exception.*;
 
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -19,10 +22,10 @@ public class GlobalExceptionHandler {
             UserCouponNotFoundException.class
     })
     public ProblemDetail handleNotFound(RuntimeException ex) {
+        log.error(ex.getMessage(), ex);
         ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
-        problem.setTitle("리소스를 찾을 수 없습니다.");
+        problem.setTitle("Resource not found");
         problem.setDetail(ex.getMessage());
-        problem.setProperty("errorCode", "NOT_FOUND");
         return problem;
     }
 
@@ -32,10 +35,10 @@ public class GlobalExceptionHandler {
             AlreadyUsedCouponException.class
     })
     public ProblemDetail handleBadRequest(RuntimeException ex) {
+        log.error(ex.getMessage(), ex);
         ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
-        problem.setTitle("잘못된 요청입니다.");
+        problem.setTitle("Bad Request");
         problem.setDetail(ex.getMessage());
-        problem.setProperty("errorCode", "BAD_REQUEST");
         return problem;
     }
 
@@ -43,9 +46,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UnauthorizedException.class)
     public ProblemDetail handleUnauthorized(UnauthorizedException ex) {
         ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.UNAUTHORIZED);
-        problem.setTitle("인증이 필요합니다.");
+        problem.setTitle("Unauthorized");
         problem.setDetail(ex.getMessage());
-        problem.setProperty("errorCode", "UNAUTHORIZED");
         return problem;
     }
 
@@ -53,43 +55,42 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UnauthorizedUserCouponAccessException.class)
     public ProblemDetail handleForbidden(UnauthorizedUserCouponAccessException ex) {
         ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.FORBIDDEN);
-        problem.setTitle("권한이 없습니다.");
+        problem.setTitle("Forbidden");
         problem.setDetail(ex.getMessage());
-        problem.setProperty("errorCode", "FORBIDDEN");
         return problem;
     }
 
-    //validation exception
+    //validationexception
     @ExceptionHandler(ValidationException.class)
     public ProblemDetail handleCustomValidation(ValidationException ex) {
         ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
-        problem.setTitle("비즈니스 유효성 검사 실패");
+        problem.setTitle("Validation_Error");
         problem.setDetail(ex.getMessage());
-        problem.setProperty("errorCode", "VALIDATION_ERROR");
         return problem;
     }
 
-    // 검증 실패
-    @ExceptionHandler(org.springframework.web.bind.MethodArgumentNotValidException.class)
+    // @vaild
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ProblemDetail handleValidation(org.springframework.web.bind.MethodArgumentNotValidException ex) {
         ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
-        problem.setTitle("검증에 실패했습니다.");
+        problem.setTitle("Validation_Error");
+        log.error(ex.getMessage(), ex);
         String detail = ex.getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .map(err -> err.getField() + " " + err.getDefaultMessage())
                 .collect(Collectors.joining(", "));
         problem.setDetail(detail);
-        problem.setProperty("errorCode", "VALIDATION_ERROR");
         return problem;
     }
 
     // 그 외
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleOther(Exception ex) {
+        log.error(ex.getMessage(), ex);
         ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-        problem.setTitle("서버 내부 오류");
-        problem.setDetail("알 수 없는 오류가 발생했습니다.");
+        problem.setTitle("Internal Server Error");
+        problem.setDetail("Internal Server Error");
         return problem;
     }
 }
