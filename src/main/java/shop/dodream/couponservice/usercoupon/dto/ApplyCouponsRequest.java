ef32b2f8068c4633eeb3dde4751d48f3b4b4b8e1
chosun-nhn17 <12160569@inha.edu>
2025-07-10 14:42:02 +0900

    feat: applycoupnsrequest add annotation
    - 기본생성자 모든 필드 생성자 추가
    
    #47

--- src/main/java/shop/dodream/couponservice/usercoupon/dto/ApplyCouponsRequest.java
+++ src/main/java/shop/dodream/couponservice/usercoupon/dto/ApplyCouponsRequest.java
@@ -2,11 +2,15 @@ package shop.dodream.couponservice.usercoupon.dto;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApplyCouponsRequest {
 
     @NotEmpty
