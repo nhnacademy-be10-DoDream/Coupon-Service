e9d678554820be7435d765b538ba3e8d8177e721
chosun-nhn17 <12160569@inha.edu>
2025-07-12 17:59:35 +0900

    bugfix: add jacksonconfig
    - 빈문자열 null 로 변경
    - 추가로 request param user 쪽과 이름 맞춤
    
    #55

---
+++ src/main/java/shop/dodream/couponservice/config/JacksonConfig.java
@@ -0,0 +1,20 @@
+package shop.dodream.couponservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+        objectMapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
+        return objectMapper;
+    }
+}
+
